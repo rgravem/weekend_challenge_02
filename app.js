244539245437549cd92f7cadb697f9168454052c
Ross Gravem <rgravem@yahoo.com>
2016-09-06 10:36:42 -0500

    fixed broken updater from last commit

--- app.js
+++ app.js
@@ -41,9 +41,9 @@ var wholePi = function () {
   }
 };
 
-var showPiece = function(piece){
+var showPiece = function(current){
   // function that fades in and fades out when display changes
-  
+  piece = current;
   console.log('in showPiece');
   $('#resultsDiv').fadeOut('slow', function(){
     $('#resultsDiv').html("<p>" + piPieces[piece].first_name + " " + piPieces[piece].last_name + "<br>" + piPieces[piece].info);
